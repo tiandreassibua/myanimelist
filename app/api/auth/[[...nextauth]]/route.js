f22462a75791bdbd46512b0698ca90f79921f496
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2024-03-12 00:12:50 +0700

    feat: remove login with github

--- app/api/auth/[[...nextauth]]/route.js
+++ app/api/auth/[[...nextauth]]/route.js
@@ -1,13 +1,13 @@
 import NextAuth from "next-auth";
-import Github from "next-auth/providers/github";
+// import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 export const OPTIONS = {
     providers: [
-        Github({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        }),
+        // Github({
+        //     clientId: process.env.GITHUB_CLIENT_ID,
+        //     clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        // }),
         Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
